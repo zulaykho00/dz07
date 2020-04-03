9cc924a617fd397c92a4109846d9592ab3b00da3
zulaykho00 <zuay2409@mail.ru>
2020-04-03 21:04:37 +0500

    класс прямоугольник

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace ddzz7
 {
+    class Rectangle
+    {
+         double side1,side2,area,perimeter;
+         public double Area{get{return area;} }
+         public double Perimeter{get{return perimeter;} }
+          
+        public Rectangle(double side1,double side2)
+        {
+            this.side1=side1;
+            this.side2=side2;
+    
+        }
+       public  void AreaCalculator()
+        {
+            area=side1*side2;
+          
+        }
+        public void PerimeterCalculator()
+        {
+           perimeter=2*(side1+side2);
+         
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        double side1,side2;
+        Console.Write("side1=");
+        side1=Convert.ToDouble(Console.ReadLine());
+        Console.Write("side2=");
+        side2=Convert.ToDouble(Console.ReadLine());
+        Rectangle ABC=new Rectangle(side1,side2);
+        ABC.AreaCalculator();
+        ABC.PerimeterCalculator();
+        Console.WriteLine(ABC.Area);
+        Console.WriteLine(ABC.Perimeter);
 
         }
     }
